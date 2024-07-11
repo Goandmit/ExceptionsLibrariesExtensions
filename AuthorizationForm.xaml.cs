b38215f1ef02008848fdb04d659ca30dfcdc25ce
Goandmit <137429299+Goandmit@users.noreply.github.com>
2024-07-11 15:07:19 +0300

    Add files via upload

---
+++ AuthorizationForm.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ExceptionsLibrariesExtensions
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationForm.xaml
+    /// </summary>
+    public partial class AuthorizationForm : Window
+    {
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+            DataContext = new AuthorizationFormVM();
+        }
+    }
+}
---
+++ ChangeLog.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ExceptionsLibrariesExtensions
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeLog.xaml
+    /// </summary>
+    public partial class ChangeLog : Window
+    {
+        public ChangeLog()
+        {
+            InitializeComponent();
+            DataContext = new ChangeLogVM();
+        }
+    }
+}
